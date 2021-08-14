bdcc8045cef4853c6dbdf610fe569de0a32072b8
ashok <pandeyashok761@gmail.com>
2021-08-14 16:35:46 +0530

    Add project files.

---
+++ MyApp.Db/DbOperations/EmployeeRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyApp.Models;
+namespace MyApp.Db.DbOperations
+{
+   public class EmployeeRepository
+    {
+        public int AddEmployee(EmployeeModel model)
+        {
+            using (var context = new EmployeeDBEntities())
+            {
+                Employee emp = new Employee();
+                {
+                    emp.FirstName = model.FirstName;
+                    emp.LastName = model.LastName;
+                    emp.Email = model.Email;
+                    emp.Code = model.Code;
+                }
+                context.Employee.Add(emp);
+                context.SaveChanges();
+                return emp.Id;
+            }
+            
+        }
+    }}
---
+++ SaveDataInDb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SaveDataInDb.MvcApplication" Language="C#" %>
